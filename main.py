d6646e82136b88e0eb3a0bb8466e17e9d4622c00
seventeenseven <ajane717@gmail.com>
2023-03-22 16:46:25 +0100

    add docstring

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ for x in range(10):
     if x > 8 :
         print(f"This is {x}")
 print("END")
+
+print("test")
+
--- test_sample.py
+++ test_sample.py
@@ -2,6 +2,7 @@
 def inc(num):
     return num + 1
 
-
+#Another function
 def test_answer():
     assert inc(3) == 5
+
